7ea8a2e06a2b21e977201edbfd0451004e6e8580
dang ha <danghongha96@gmail.com>
2020-11-26 21:58:30 +0700

    Initial commit

---
+++ testcases/com/bankguru/user/TC_01_Repeat_Yourself.java
@@ -0,0 +1,36 @@
+package com.bankguru.user;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TC_01_Repeat_Yourself {
+WebDriver driver;
+String projectPath= System.getProperty("user.dir");
+    @BeforeClass
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", projectPath+
+                "\\browserDriver\\chromedriver.exe");
+        driver= new ChromeDriver();
+        driver.get("http://www.demo.guru99.com/v4/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+
+    }
+    @Test
+    public void f(){
+
+    }
+
+    @AfterClass
+    public void afterClass(){
+
+    }
+
+}
---
+++ testcases/com/bankguru/user/TC_02_Abstract_Page.java
@@ -0,0 +1,23 @@
+package com.bankguru.user;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TC_02_Abstract_Page {
+
+    @BeforeClass
+    public void setUp(){
+
+    }
+    @Test
+    public void f(){
+
+    }
+
+    @AfterClass
+    public void afterClass(){
+
+    }
+
+}
